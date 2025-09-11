521fa57bef578b7f4d08ce5028da9cdfcc269868
Ho-67 <ho67cnl@gmail.com>
2025-09-12 04:32:24 +0800

    Rename NovelSpotlight

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/NOVELSPOTLIGHT/",
+  base: "/NovelSpotlight/",
   plugins: [vue(), Pages(), tailwindcss()],
   resolve: {
     alias: {
